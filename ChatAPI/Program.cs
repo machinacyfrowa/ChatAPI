7d7523cb4a26ec3f5d58de67260f3f179bfd6c2b
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-10-13 13:24:04 +0200

    Dodano obsługę timestamp jako string w API czatu
    Wprowadzono nową przestrzeń nazw `System.Web` oraz skonfigurowano adres IP i port aplikacji. Zmieniono typ parametru `timestamp` w metodzie `MapGet` z `DateTime?` na `string?`, co umożliwia przyjmowanie wartości timestamp jako ciągu znaków. Dodano logikę do parsowania ciągu do obiektu `DateTime` oraz wprowadzono komentarze wyjaśniające zmiany i logikę działania kodu.

--- ChatAPI/Program.cs
+++ ChatAPI/Program.cs
@@ -1,7 +1,10 @@
 using ChatAPI;
+using System.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+//ustaw adresy ip i porty
+app.Urls.Add("http://192.168.6.25:5000");
 
 ChatHistory chatHistory = new ChatHistory();
 chatHistory.AddMessage(new ChatMessage("User", "Hello!"));
@@ -12,12 +15,21 @@ chatHistory.AddMessage(new ChatMessage("User", "Can you tell me a joke?"));
 app.MapGet("/", () => "Hello World!");
 
 
-app.MapGet("/chat", (DateTime? timestamp) =>
+app.MapGet("/chat", (string? timestamp) =>
 {
+    //timestamp jest null tylko wtedy kiedy jawnie odpytamy bez niego
+    //a nie na przyk³ad na skutek b³êdu
     if (timestamp == null)
         return chatHistory.GetLast(10);
     //tutaj zwracamy historiê czatu
-    return chatHistory.GetMessagesAfter(timestamp ?? DateTime.Now);
+    //ustawiamy defaultowy czas ostatniej wiadomoœci na teraz
+    //nie jest to zbyt logiczne ale potrzebujemy pocz¹tkowej wartoœci
+    DateTime parsedTimestamp = DateTime.Now;
+    //timestamp jest w postaci stringa "2025-10-13T13:08:22.1712280+02:00"
+    //parsujemy go do formatu obiektu datetime
+    DateTime.TryParse(timestamp, out parsedTimestamp);
+    //odpytujemy nasz¹ klasê o wiadomoœci po danym timestampie i wysy³amy
+    return chatHistory.GetMessagesAfter(parsedTimestamp);
 });
 app.MapPost("/chat", (ChatMessage message) =>
 {
